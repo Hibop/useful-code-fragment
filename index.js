0d314e50b885414533bb400781182b4c334585ac
Hibop Yuan <862231149@qq.com>
2018-11-08 21:30:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+/**
+ * 将es6数组遍历相关方法，扩展到对象中
+ * 使用示例：
+ * ({k: 1, d: 3}).map((value, key, obj) => value) // [1, 3]
+ */
 
+Object.prototype.map = function(fn) {
+  return Object.keys(this).map(k => {
+    return fn.call(this, this[k], k, this)
+  })
+}
+
+// 函数式写法: 最外层的function不能使用箭头函数，否则this => window
+Object.prototype.map = function(fn) {
+return Object.keys(this).map(
+  k => fn.call(this, this[k], k, this)
+)
+}
+// reduce方法
+Object.prototype.reduce = function (fn, init) {
+  return Object.keys(this).reduce(
+    (r, k, i) => fn.call(this, r, this[k], k, this),
+    init
+  )
+}
