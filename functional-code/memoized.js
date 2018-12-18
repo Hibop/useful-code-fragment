d380ffb1b0e46dbdb9d0fd5cfb93425a316ca7b9
Hibop Yuan <862231149@qq.com>
2018-12-18 19:36:54 +0800

    Create memoized.js

---
+++ functional-code/memoized.js
@@ -0,0 +1,12 @@
+// 记忆函数
+const memoized = (fn) => {
+  const mTable= {};
+  return (arg) => mTable[arg] || (mTable[arg] = fn(arg));
+}
+
+export default {memoized};
+
+// example 记忆函数加快阶乘速度
+const factorial = n => n === 0 ? 1 : n * factorial(n-1);
+
+const fastfactorial = memoized(factorial);
