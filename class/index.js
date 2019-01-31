73b07ff8dda5044b907033e70c06c8b1b14c1994
Hibop Yuan <862231149@qq.com>
2019-01-31 17:50:24 +0800

    Create index.js

---
+++ class/index.js
@@ -0,0 +1,14 @@
+const div = document.createElemnt('div');
+div.className = 'foo';
+// 增加类
+div.classList.add('another');
+// 删除
+div.classList.remove('foo');
+// 
+div.classList.toggle('foo');
+
+// has
+div.classList.contains('foo');
+
+div.classList.replace('foo');
+
