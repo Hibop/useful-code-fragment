721e3284803aa50badb12a734ab00fda81966ab1
Hibop Yuan <862231149@qq.com>
2019-03-12 21:23:13 +0800

    Update bind.js

--- functional-code/bind.js
+++ functional-code/bind.js
@@ -2,6 +2,7 @@
 // 1. 保存this返回一个函数
 // 2. 传入参数
 // 3. 可以重新new
+// link https://juejin.im/post/5bec4183f265da616b1044d7/
 Function.prototype.nBind = function(context, ...args) {
   var self = this; // this是实际的实例函数
   // Array.prototype.slice(arguments, 1)
@@ -11,6 +12,7 @@ Function.prototype.nBind = function(context, ...args) {
 //     this.call(context)
     const a = [...args, ...argsAfter];
 //     return self.apply(context, a);
+    // 保证的new和非new情况
     var ctx = this instanceof self ? this : context;
     return self.apply(ctx, a); 
   }
