1644df8d4c6176f1fceeb001f6cde6dbbda60920
Hibop Yuan <862231149@qq.com>
2019-06-11 11:23:00 +0800

    Update unary.js

--- functional-code/unary.js
+++ functional-code/unary.js
@@ -1,1 +1,7 @@
+// 将接受多参数函数转化为只接受单参数函数
+const unary = fn => fn.length <= 1 ? fn : (arg) => fn(arg);
+export default unary
 
+// test
+const arrInt  = [1,2,3].map(parseInt) // => [1, NaN, NaN];
+const arrInt2 = [1,2,3].map(unary(parseInt)); // [1, 2, 3]
