bc6ce8ba3ea62ac67adfb8ec91438e87bb670d3f
Hibop Yuan <862231149@qq.com>
2019-06-24 13:59:50 +0800

    Update visibilitychange.js

--- business/visibilitychange.js
+++ business/visibilitychange.js
@@ -1,3 +1,16 @@
-export default{
-
+export default {
+  mounted: {
+    document.addEventListener('visibilitychange', this.handleVibility)
+  },
+  
+  destoryed: {
+    document.removeEventListener('visibilitychange', this.handleVisibiliy)
+  },
+  
+  methods: {
+    handleVibility() {
+      if(document.visibilityState === 'hidden') return;
+      this.fetchList();
+    }
+  }
 }
