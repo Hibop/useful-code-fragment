f034122a9292bf7cabb1d14ebc22e7d6ab42f7ad
Hibop Yuan <862231149@qq.com>
2019-07-03 14:34:10 +0800

    Update 优雅处理Async-Await参数.js

--- async-await/优雅处理Async-Await参数.js
+++ async-await/优雅处理Async-Await参数.js
@@ -2,4 +2,9 @@ function AsyncTo(promise) {
     return promise.then(data => [null, data]).catch(err => [err]);
 }
 
-const [err, res] = await AsyncTo(Func());
+// 使用
+async function fetchData(url) {
+    const [err, data] = await AsyncTo(fetchApi(url));
+    return err ? err : data;
+}
+
