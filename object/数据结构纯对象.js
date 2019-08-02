860fad1bf29c854133baa89e598a20d76f70f05b
Hibop Yuan <862231149@qq.com>
2019-08-02 18:31:03 +0800

    Update 数据结构纯对象.js

--- object/数据结构纯对象.js
+++ object/数据结构纯对象.js
@@ -28,3 +28,33 @@ console.timeEnd('obj'); // obj: 0.051025390625ms
 console.time('pure');
 result = pure[123123];
 console.timeEnd('pure'); // pure: 0.0068359375ms
+
+
+// ------------------------------------------------------------------------------
+//   纯数组
+// -----------------------------------------------------------------------------
+function pureArray(arr = [], ...args) {
+  const newArr = Object.assign([], arr); // 保证对原数组无副作用
+  Array.prototype.push.call(newArr, ...args);
+  newArr.__proto__ = null;
+  return newArr; // 返回为
+}
+
+let arr = [],
+  pure = pureArray([]),
+  n = 10000000;
+for (let i = 0; i < n; i++) {
+  arr.push(i);
+  Array.prototype.push.call(pure, i);
+}
+
+console.time('arr');
+result = arr[123123];
+console.timeEnd('arr'); // arr: 0.083984375ms
+
+console.time('pure');
+result = pure[123123];
+console.timeEnd('pure'); // pure: 0.005126953125ms
+
+
+
