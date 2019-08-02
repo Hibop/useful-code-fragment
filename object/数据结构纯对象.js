1b3f561dd164d55d7ca72aed3ed938eaf043c0f8
Hibop Yuan <862231149@qq.com>
2019-08-02 15:59:43 +0800

    Update 数据结构纯对象.js

--- object/数据结构纯对象.js
+++ object/数据结构纯对象.js
@@ -1,4 +1,4 @@
-export function pureObj(...args) {
+function pureObj(...args) {
   const obj = Object.create(null);
   const mergeArgs = Object.assign({}, ...args);
 
@@ -7,6 +7,19 @@ export function pureObj(...args) {
       obj[key] = mergeArgs[key];
     }
   }
-
   return obj;
 }
+
+let obj = {}, pure = pureObj({}), n = 10000000;
+for (let i = 0; i < n; i++) {
+  obj[i] = i;
+  pure[i] = i;
+}
+
+console.time('obj');
+result = obj[123123];
+console.timeEnd('obj'); // obj: 0.051025390625ms
+
+console.time('pure');
+result = pure[123123];
+console.timeEnd('pure'); // pure: 0.0068359375ms
