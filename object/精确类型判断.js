4eca4df271d2be5385bf0948690bd2b4cd12b9b6
Hibop Yuan <862231149@qq.com>
2019-08-02 17:05:54 +0800

    Create 精确类型判断.js

---
+++ object/精确类型判断.js
@@ -0,0 +1,28 @@
+function typeOf(obj) {
+  const toString = Object.prototype.toString;
+  var map = new Map()
+    .set('[object Undefined]',    'undefined')
+    .set('[object Null]',         'null')
+    .set('[object Boolean]',      'boolean')
+    .set('[object Number]',       'number')
+    .set('[object String]',       'string')
+    .set('[object Function]',     'function')
+    .set('[object Array]',        'array')
+    .set('[object Date]',         'date')
+    .set('[object Object]',       'object')
+    .set('[object RegExp]',       'regExp')
+    .set('[object global]',       'window')
+    .set('[object Math]',         'math')
+    .set('[object JSON]',         'json')
+    .set('[object Set]',          'set')
+    .set('[object Map]',          'map')
+    .set('[object HTMLDocument]', 'document');
+
+  return map.get(toString.call(obj));
+}
+
+// test
+typeOf('s')   // 'string'
+typeOf({})   // 'object'
+typeOf(document)   // 'document'
+typeOf(new Set())   // 'set'
