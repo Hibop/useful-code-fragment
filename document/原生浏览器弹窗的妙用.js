d79092e852706ccaeb89ede3d45db9b6a0c8e18a
Hibop Yuan <862231149@qq.com>
2019-11-28 17:41:14 +0800

    Update 原生浏览器弹窗的妙用.js

--- document/原生浏览器弹窗的妙用.js
+++ document/原生浏览器弹窗的妙用.js
@@ -6,5 +6,39 @@ window.onbeforeunload = function(e) {
   return dialogText;
 }
 
-// 
+// 浏览器主动推送信息流 提示 
+// Notification
+var options = {
+  dir: "auto", // 文字方向
+  body: "通知：sssssssssssssssssssssss", // 通知主体
+  requireInteraction: true, // 不自动关闭通知
+};
+notifyMe('这是通知的标题', options /* options 配置比较多 可以查看mdn */);
+function notifyMe(title, options) {
+  // 先检查浏览器是否支持
+  if (!window.Notification) {
+    console.log('浏览器不支持通知');
+  } else {
+    // 检查用户曾经是否同意接受通知
+    if (Notification.permission === 'granted') {
+      var notification = new Notification(title, options); // 显示通知
+    } else if (Notification.permission === 'default') {
+      // 用户还未选择，可以询问用户是否同意发送通知
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('用户同意授权');
+          var notification = new Notification(title, options); // 显示通知
+        } else if (permission === 'default') {
+          console.warn('用户关闭授权 未刷新页面之前 可以再次请求授权');
+        } else {
+          // denied
+          console.log('用户拒绝授权 不能显示通知');
+        }
+      });
+    } else {
+      // denied 用户拒绝
+      console.log('用户曾经拒绝显示通知');
+    }
+  }
+}
 
