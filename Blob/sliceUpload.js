a3b4f559b1bc3ecdfbc61ece4cd50d301d087f96
Hibop Yuan <862231149@qq.com>
2021-04-21 14:57:45 +0800

    Create sliceUpload.js

---
+++ Blob/sliceUpload.js
@@ -0,0 +1,23 @@
+function upload(blobOrFile) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/server', true);
+  xhr.onload = function(e) { ... };
+  xhr.send(blobOrFile);
+}
+
+document.querySelector('input[type="file"]').addEventListener('change', function(e) {
+  var blob = this.files[0];
+
+  const BYTES_PER_CHUNK = 1024 * 1024; // 1MB chunk sizes.
+  const SIZE = blob.size;
+
+  var start = 0;
+  var end = BYTES_PER_CHUNK;
+
+  while(start < SIZE) {
+    upload(blob.slice(start, end));
+
+    start = end;
+    end = start + BYTES_PER_CHUNK;
+  }
+}, false);
