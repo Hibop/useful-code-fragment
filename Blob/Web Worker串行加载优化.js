9e58eb5f58b93f53e31b7329c239ab5ebfa2977c
Hibop Yuan <862231149@qq.com>
2021-04-21 15:18:57 +0800

    Update Web Worker串行加载优化.js

--- Blob/Web Worker串行加载优化.js
+++ Blob/Web Worker串行加载优化.js
@@ -1,7 +1,7 @@
 // https://www.chyingp.com/posts/how-to-avoid-web-worker-from-sequential-loading/
 // https://cloud.tencent.com/developer/article/1651215
 
-// 
+// node-gen脚本
 const fs = require('fs');
 const workerFilePath = './worker.js';
 const mainFilePath = './main.js';
@@ -13,4 +13,15 @@ const bundleFileContent = mainFileContent.replace('./worker.js', workerFileConte
 
 fs.writeFileSync(bundleFilePath, bundleFileContent);
 
+// main-thread
+const workerFileContent = `./worker.js`;
+const workerBlob = new Blob([workerFileContent], { type:'text/javascript' });
+const workerUrl = URL.createObjectURL(workerBlob);
+const worker = new Worker(workerUrl);
 
+worker.addEventListener('message', function(evt) {
+    console.log(`[main] result is: ${evt.data.result}.`);
+}, false);
+
+worker.postMessage({num1: 20, num2: 10});
+console.log('[main] Main is initialized.');
